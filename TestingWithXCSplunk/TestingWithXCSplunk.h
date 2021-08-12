2f44e7dbea6e215c10a3c03a762416d3011592e8
Zindal <jindalp@splunk.com>
2021-08-12 23:32:31 +0530

    Initial Commit

---
+++ TestingWithXCSplunk/TestingWithXCSplunk.h
@@ -0,0 +1,12 @@
+//
+//  TestingWithXCSplunk.h
+//  TestingWithXCSplunk
+//
+//  Created by Jindal Patel on 12/08/21.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestingWithXCSplunk : NSObject
+
+@end
---
+++ TestingWithXCSplunk/TestingWithXCSplunk.m
@@ -0,0 +1,12 @@
+//
+//  TestingWithXCSplunk.m
+//  TestingWithXCSplunk
+//
+//  Created by Jindal Patel on 12/08/21.
+//
+
+#import "TestingWithXCSplunk.h"
+
+@implementation TestingWithXCSplunk
+
+@end
